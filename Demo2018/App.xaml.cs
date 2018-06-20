dda887ebb9ea46580a0c69af479bcb8f694613b0
hungnd <hungnd@runsystem.net>
2018-06-20 16:42:24 +0700

    no message

--- Demo2018/App.xaml.cs
+++ Demo2018/App.xaml.cs
@@ -3,6 +3,8 @@ using Demo2018.Logics.ViewModels.MainPage;
 using Demo2018.Logics.ViewModels.Rentals;
 using Demo2018.Logics.ViewModels.Search;
 using Demo2018.Views.Screens.MainPage;
+using Demo2018.Views.Screens.Rentals;
+using Demo2018.Views.Screens.Search;
 using Prism.Ioc;
 using Prism.Unity;
 using Xamarin.Forms;
@@ -27,6 +29,8 @@ namespace Demo2018
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<MainPageScreen, MainPageViewModel>("mainPage");
+            containerRegistry.RegisterForNavigation<SearchScreen, SearchViewModel>("searchPage");
+            containerRegistry.RegisterForNavigation<RentalsScreen, RentalViewModel>("rentalPage");
         }
 
         protected override void OnStart()
