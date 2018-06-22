7bcbd554a3e5e1585fba62d380d888db20eca656
hungnd <hungnd@runsystem.net>
2018-06-22 16:42:58 +0700

    fix button and animation for button

--- Demo2018.Views/Screens/Search/SearchScreen.xaml.cs
+++ Demo2018.Views/Screens/Search/SearchScreen.xaml.cs
@@ -10,6 +10,28 @@ namespace Demo2018.Views.Screens.Search
         public SearchScreen()
         {
             InitializeComponent();
+            var tapGestureRecognizer = new TapGestureRecognizer();
+        }
+
+        async void BikeButtonTapped(object sender, System.EventArgs e)
+        {
+            stBikeButton.Opacity = 1;
+            await stBikeButton.FadeTo(0.5, 100);
+            await stBikeButton.FadeTo(1, 100);
+        }
+
+        async void LocationButtonTapped(object sender, System.EventArgs e)
+        {
+            stLocationButton.Opacity = 1;
+            await stLocationButton.FadeTo(0.5, 100);
+            await stLocationButton.FadeTo(1, 100);
+        }
+
+        async void CheckListButtonTapped(object sender, System.EventArgs e)
+        {
+            stCheckListButton.Opacity = 1;
+            await stCheckListButton.FadeTo(0.5, 100);
+            await stCheckListButton.FadeTo(1, 100);
         }
     }
 }
