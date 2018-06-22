588589f499e5dff553ce2c8d6321f1c7def11917
hungnd <hungnd@runsystem.net>
2018-06-22 16:49:49 +0700

    fix multi click crash app

--- Demo2018.Views/Screens/Search/SearchScreen.xaml.cs
+++ Demo2018.Views/Screens/Search/SearchScreen.xaml.cs
@@ -7,6 +7,7 @@ namespace Demo2018.Views.Screens.Search
 {
     public partial class SearchScreen : BaseScreen
     {
+        bool isClicked = false;
         public SearchScreen()
         {
             InitializeComponent();
@@ -15,23 +16,35 @@ namespace Demo2018.Views.Screens.Search
 
         async void BikeButtonTapped(object sender, System.EventArgs e)
         {
+            if (isClicked)
+                return;
+            isClicked = true;
             stBikeButton.Opacity = 1;
             await stBikeButton.FadeTo(0.5, 100);
             await stBikeButton.FadeTo(1, 100);
+            isClicked = false;
         }
 
         async void LocationButtonTapped(object sender, System.EventArgs e)
         {
+            if (isClicked)
+                return;
+            isClicked = true;
             stLocationButton.Opacity = 1;
             await stLocationButton.FadeTo(0.5, 100);
             await stLocationButton.FadeTo(1, 100);
+            isClicked = false;
         }
 
         async void CheckListButtonTapped(object sender, System.EventArgs e)
         {
+            if (isClicked)
+                return;
+            isClicked = true;
             stCheckListButton.Opacity = 1;
             await stCheckListButton.FadeTo(0.5, 100);
             await stCheckListButton.FadeTo(1, 100);
+            isClicked = false;
         }
     }
 }
