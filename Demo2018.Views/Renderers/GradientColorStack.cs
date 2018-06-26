7213330814a1718e867719e3a9b1cce0b30d1590
Truc Nguyen <trucntt@runsystem.net>
2018-06-26 14:15:47 +0700

    no message

---
+++ Demo2018.Views/Renderers/GradientColorStack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Demo2018.Views.Renderers
+{
+    public class GradientColorStack : Grid
+    {
+        public Color StartColor { get; set; }
+        public Color EndColor { get; set; }
+    }
+}
